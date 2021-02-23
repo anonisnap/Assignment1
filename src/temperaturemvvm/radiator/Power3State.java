fc5f3874a7e76b97e7c17f918c91e95cba78df32
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-02-23 18:19:47 +0100

    Thread now set to Daemon

--- src/temperaturemvvm/radiator/Power3State.java
+++ src/temperaturemvvm/radiator/Power3State.java
@@ -7,12 +7,13 @@ public class Power3State implements RadiatorState {
 	public Power3State(Radiator r) {
 		t = new Thread(() -> {
 				try {
-					Thread.sleep(10000);
+					Thread.sleep(40000);
 					r.setPowerState(new Power2State());
 				} catch (InterruptedException e) {
 					// Likely turned off by User
 			}
 		});
+		t.setDaemon(true);
 		t.start();
 	}
 
